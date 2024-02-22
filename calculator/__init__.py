f972d49b8054aa441fc9e83da7be0dca40a4c21b
jcas1 <johncastillo412@gmail.com>
2024-02-21 19:16:23 -0500

    changes

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,3 +1,4 @@
+# pylint: disable=line-too-long
 '''Operations'''
 from calculator.operations import addition, subtraction, multiplication, division
 from calculator.calculation import Calculation
@@ -7,23 +8,23 @@ class Calculator:
     @staticmethod
     def add(a,b):
         '''Returns the sum of the two values'''
-        calc = Calculation(a,b,addition) #Passes two numbers into the addition function
+        calc = Calculation(a, b, addition) #Passes two numbers into the addition function
         return calc.get_result()
 
     @staticmethod
     def sub(a,b):
         '''Returns the difference of the two values'''
-        calc = Calculation(a,b,subtraction) #Passes two numbers into the subtraction function
+        calc = Calculation(a, b, subtraction) #Passes two numbers into the subtraction function
         return calc.get_result()
 
     @staticmethod
     def multiply(a,b):
         '''Returns the product of the two values'''
-        calc = Calculation(a,b,multiplication) #Passes two numbers into the multiplication function
+        calc = Calculation(a, b, multiplication) #Passes two numbers into the multiplication function
         return calc.get_result()
 
     @staticmethod
     def divide(a,b):
         '''Returns the quotient of the two values'''
-        calc = Calculation(a,b,division) #Passes two numbers into the division function
+        calc = Calculation(a, b, division) #Passes two numbers into the division function
         return calc.get_result()
